1f9259873463648352546c7b89ca4865cd95c2f3
HeavyBreathingCat <bosquearenas@hotmail.com>
2020-04-20 18:07:37 +0200

    Constructors and class rename

--- src/character/Paladin.java
+++ src/character/Paladin.java
@@ -2,7 +2,7 @@ package character;
 
 import java.util.*;
 
-public class Character_Paladin extends Character {
+public class Paladin extends Character {
 
 	public static float racial_bleed_mult = (float) 0.8;
 	
@@ -10,9 +10,26 @@ public class Character_Paladin extends Character {
 		put("attributes", new HashMap<String, Integer>());
 		put("weapon_skills", new HashMap<String, Integer>());
 		put("combat_skills", new HashMap<String, Integer>());			
-		put("armor_skills", new HashMap<String, Integer>());
 	}};
 
+	public Paladin (String name) {
+		this.name = name;
+	}
+	
+	public Paladin (String name, Map<String, Integer> attributes, Map <String, Integer> weapon_skills, Map<String, Integer> combat_skills) {
+		this.name = name;
+		
+		// Update stats dict
+		for (Map.Entry<String, Integer> pair : attributes.entrySet()) {
+			this.stats.get("attributes").put(pair.getKey(), pair.getValue());
+		} for (Map.Entry<String, Integer> pair : weapon_skills.entrySet()) {
+			this.stats.get("weapon_skills").put(pair.getKey(), pair.getValue());
+		} for (Map.Entry<String, Integer> pair : combat_skills.entrySet()) {
+			this.stats.get("combat_skills").put(pair.getKey(), pair.getValue());
+		} 
+		
+	}
+	
 	@Override
 	public Map<String, Integer> getAttributes() {
 		// TODO Auto-generated method stub
