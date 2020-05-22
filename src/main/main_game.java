0a7e61699a063707ba2060b2332379277f6dad3f
HeavyBreathingCat <bosquearenas@hotmail.com>
2020-05-22 18:49:03 +0200

    And or i am done

--- src/main/main_game.java
+++ src/main/main_game.java
@@ -184,7 +184,7 @@ public class main_game {
 		
 		System.out.println(enemies.toString());
 
-		while(new main_game().teamAlive(player_characters) || new main_game().enemyTeamAlive(enemies)) {
+		while(new main_game().teamAlive(player_characters) && new main_game().enemyTeamAlive(enemies)) {
 			// Player starts
 			for(Character player_char : player_characters) {
 				if(player_char.isAlive()) {
